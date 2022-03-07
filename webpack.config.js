b2030315d3356bd9af429880b550806d5869db74
Jon Rojí <joroji@MacBook-Pro-de-Jon.local>
2022-03-07 15:53:58 +0100

    updates on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, ''),
+    entry: {
+      index: './src/index.js',
     },
-    compress: true,
-    port: 9000,
-  },
+    devServer: {
+        static: path.join(__dirname, '/'),
+        compress: true,
+        port: 9000,
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
 };
